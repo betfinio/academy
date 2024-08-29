5ce4dd8f897dceecb732d57a1ef9d8a2558642f6
Sanni <sannikov.matt@proton.me>
2024-08-29 17:33:39 +0200

    Added path check and styles for mobile view

--- src/routes/_index.tsx
+++ src/routes/_index.tsx
@@ -1,48 +1,54 @@
-import { Link, Outlet, createFileRoute, useLocation } from '@tanstack/react-router';
-import { cx } from 'class-variance-authority';
-import { BookIcon, CalendarHeart, GraduationCap, PencilRulerIcon } from 'lucide-react';
-import { useTranslation } from 'react-i18next';
+import { Link, Outlet, createFileRoute, useLocation } from "@tanstack/react-router";
+import { cx } from "class-variance-authority";
+import { BookIcon, CalendarHeart, GraduationCap, PencilRulerIcon } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
-export const Route = createFileRoute('/_index')({
-	component: () => <Layout />,
+export const Route = createFileRoute("/_index")({
+  component: () => <Layout />,
 });
 
 function Layout() {
-	const { t } = useTranslation('', { keyPrefix: 'academy.layout' });
-	const router = useLocation();
-	const isActive = (path: string) => {
-		return router.pathname.includes(path);
-	};
-	const getIcon = (path: string) => {
-		switch (path) {
-			case 'new':
-				return <GraduationCap className={'w-6 h-6 '} />;
-			case 'advanced':
-				return <PencilRulerIcon className={'w-5 h-5 '} />;
-			case 'events':
-				return <CalendarHeart className={'w-5 h-5 '} />;
-			case 'docs':
-				return <BookIcon className={'w-5 h-5 '} />;
-		}
-	};
-	return (
-		<div className={'p-2 md:p-3 lg:p-4 text-white h-full flex flex-col gap-10 md:gap-3 lg:gap-10'}>
-			<div className={'grid grid-cols-2 md:grid-cols-4 w-full gap-2 md:gap-3 lg:gap-4  rounded-xl '}>
-				{['docs', 'new', 'advanced', 'events'].map((link) => (
-					<Link
-						to={`/${link}`}
-						key={link}
-						className={cx(
-							'flex py-2 lg:p-2 justify-center font-semibold items-center gap-2   rounded-xl duration-200 hover:bg-secondary/50 ',
-							isActive(link) ? 'text-yellow-400 bg-primary-gradient' : 'text-gray-400 ',
-						)}
-					>
-						{getIcon(link)}
-						{t(link)}
-					</Link>
-				))}
-			</div>
-			<Outlet />
-		</div>
-	);
+  const { t } = useTranslation("", { keyPrefix: "academy.layout" });
+  const router = useLocation();
+  const isActive = (path: string) => {
+    switch (path) {
+      case "new":
+        return router.href.includes("lesson/1");
+      case "advanced":
+        return /lesson\/([2-9]|\d{2,})(\/|$)/.test(router.href) || router.pathname.includes(path);
+      default:
+        return router.pathname.includes(path);
+    }
+  };
+  const getIcon = (path: string) => {
+    switch (path) {
+      case "new":
+        return <GraduationCap className={"w-6 h-6 "} />;
+      case "advanced":
+        return <PencilRulerIcon className={"w-5 h-5 "} />;
+      case "events":
+        return <CalendarHeart className={"w-5 h-5 "} />;
+      case "docs":
+        return <BookIcon className={"w-5 h-5 "} />;
+    }
+  };
+  return (
+    <div className={"p-2 md:p-3 lg:p-4 text-white h-full flex flex-col gap-10 md:gap-3 lg:gap-10"}>
+      <div className={"grid grid-cols-4 md:grid-cols-4 w-full gap-2 md:gap-3 lg:gap-4  rounded-xl "}>
+        {["docs", "new", "advanced", "events"].map((link) => (
+          <Link
+            to={`/${link}`}
+            key={link}
+            className={cx(
+              "flex flex-col md:flex-row text-xs md:text-base py-2 lg:p-2 justify-center font-semibold items-center gap-2   rounded-xl duration-200 hover:bg-secondary/50 ",
+              isActive(link) ? "bg-primary-gradient" : "text-gray-400 "            )}
+          >
+            <span className={cx(isActive(link) ? "text-yellow-400" : "text-gray-400 ")}>{getIcon(link)}</span>
+            {t(link)}
+          </Link>
+        ))}
+      </div>
+      <Outlet />
+    </div>
+  );
 }
