b51b7a4f167ee9a493705e9d45a5686edfa6eacb
Plane <bf.plane@proton.me>
2024-11-06 13:59:43 +0100

    add ability to create lessons with videos and titles on czech language

--- src/components/Lesson/LessonLeft.tsx
+++ src/components/Lesson/LessonLeft.tsx
@@ -18,11 +18,12 @@ import {
 	thematicBreakPlugin,
 } from '@mdxeditor/editor';
 import { useRouter } from '@tanstack/react-router';
+import { Button } from 'betfinio_app/button';
 import { Tooltip, TooltipContent, TooltipTrigger } from 'betfinio_app/tooltip';
 import { cx } from 'class-variance-authority';
 import { motion } from 'framer-motion';
 import { ArrowLeft, CheckIcon, Loader, TriangleIcon } from 'lucide-react';
-import { useEffect, useRef } from 'react';
+import { useEffect, useMemo, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import ReactPlayer from 'react-player/lazy';
 
@@ -36,7 +37,27 @@ export const LessonLeft = () => {
 
 	const content = JSON.parse(lessonData?.content || '{}');
 	const title = JSON.parse(lessonData?.title || '{}');
+
 	const markdown = content[i18n.language] || '';
+	const onlyEn = useMemo(() => {
+		if (!markdown && content.en) {
+			return true;
+		}
+		return false;
+	}, [content, markdown]);
+
+	const video = useMemo(() => {
+		if (lessonData?.video) {
+			try {
+				const parsedVideo = JSON.parse(lessonData.video);
+				return parsedVideo[i18n.language ?? 'en'];
+			} catch {
+				return lessonData.video;
+			}
+		} else {
+			return '';
+		}
+	}, [lessonData, i18n.language]);
 
 	useEffect(() => {
 		if (lessonData) {
@@ -52,65 +73,88 @@ export const LessonLeft = () => {
 	};
 
 	return (
-		<motion.div initial={{ opacity: 1 }} animate={{ opacity: 1 }} transition={{ duration: 0.3, delay: 0 }}>
-			<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 1 }}>
-				<div onClick={onBack} className={cx('flex cursor-pointer gap-1 items-center text-[#6A6F84] hover:text-yellow-400 group', section === '1' && 'hidden')}>
-					<ArrowLeft height={18} className={'group-hover:-translate-x-[3px] duration-300'} />
-					<span className={'duration-300'}>{t('back')}</span>
+		<div className={'relative'}>
+			{onlyEn && (
+				<div className={'absolute text-center top-1/2 -translate-y-1/2 flex flex-col gap-5 z-[10] items-center w-full'}>
+					{t('onlyEn')}
+					<Button
+						onClick={() => {
+							i18n.changeLanguage('en');
+						}}
+					>
+						{t('changeToEn')}
+					</Button>
 				</div>
-			</motion.div>
-			<div className={cx(section !== '1' && 'mt-4')}>
-				<motion.div
-					initial={{ opacity: 0, y: 20 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ duration: 0.5, delay: 0.1 }}
-					className={'text-[24px] leading-[24px] font-semibold'}
-				>
-					<div className={'flex gap-2 items-center'}>
-						<span>{title[i18n.language] || title.en}</span>
-						<Tooltip>
-							<TooltipTrigger>
-								<CheckIcon className={'text-green-400 w-5 h-5'} />
-							</TooltipTrigger>
-							<TooltipContent>{t('tooltip.completed')}</TooltipContent>
-						</Tooltip>
+			)}
+
+			<motion.div
+				className={cx(onlyEn && 'blur pointer-events-none')}
+				initial={{ opacity: 1 }}
+				animate={{ opacity: 1 }}
+				transition={{ duration: 0.3, delay: 0 }}
+			>
+				<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 1 }}>
+					<div
+						onClick={onBack}
+						className={cx('flex cursor-pointer gap-1 items-center text-[#6A6F84] hover:text-yellow-400 group', section === '1' && 'hidden')}
+					>
+						<ArrowLeft height={18} className={'group-hover:-translate-x-[3px] duration-300'} />
+						<span className={'duration-300'}>{t('back')}</span>
 					</div>
 				</motion.div>
-				<motion.div
-					initial={{ opacity: 0, y: 20 }}
-					animate={{ opacity: 1, y: 0 }}
-					transition={{ duration: 0.5, delay: 0.2 }}
-					className={cx('mt-4 w-full bg-quiz-background rounded-[10px] aspect-video overflow-hidden', !lessonData.video && 'hidden')}
-				>
-					<ReactPlayer url={lessonData.video} light={true} width="100%" height="100%" controls={true} playing={true} playIcon={<PlayButton />} />
-				</motion.div>
-			</div>
-			{lessonData.content && (
-				<motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.6 }} className={'mt-6 text'}>
-					<MDXEditor
-						ref={mdRef}
-						className={'dark-theme editor'}
-						markdown={markdown}
-						readOnly
-						plugins={[
-							headingsPlugin(),
-							linkPlugin(),
-							quotePlugin(),
-							thematicBreakPlugin(),
-							linkDialogPlugin(),
-							diffSourcePlugin(),
-							directivesPlugin({ directiveDescriptors: [AdmonitionDirectiveDescriptor] }),
-							imagePlugin(),
-							thematicBreakPlugin(),
-							tablePlugin(),
-							listsPlugin(),
-						]}
-					/>
-				</motion.div>
-			)}
-			<Quiz />
-			<Validation />
-		</motion.div>
+				<div className={cx(section !== '1' && 'mt-4')}>
+					<motion.div
+						initial={{ opacity: 0, y: 20 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.5, delay: 0.1 }}
+						className={'text-[24px] leading-[24px] font-semibold'}
+					>
+						<div className={'flex gap-2 items-center'}>
+							<span>{title[i18n.language] || title.en}</span>
+							<Tooltip>
+								<TooltipTrigger>
+									<CheckIcon className={'text-green-400 w-5 h-5'} />
+								</TooltipTrigger>
+								<TooltipContent>{t('tooltip.completed')}</TooltipContent>
+							</Tooltip>
+						</div>
+					</motion.div>
+					<motion.div
+						initial={{ opacity: 0, y: 20 }}
+						animate={{ opacity: 1, y: 0 }}
+						transition={{ duration: 0.5, delay: 0.2 }}
+						className={cx('mt-4 w-full bg-quiz-background rounded-[10px] aspect-video overflow-hidden', !video && 'hidden')}
+					>
+						<ReactPlayer url={video} light={true} width="100%" height="100%" controls={true} playing={true} playIcon={<PlayButton />} />
+					</motion.div>
+				</div>
+				{lessonData.content && (
+					<motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: 0.6 }} className={'mt-6 text'}>
+						<MDXEditor
+							ref={mdRef}
+							className={'dark-theme editor'}
+							markdown={markdown}
+							readOnly
+							plugins={[
+								headingsPlugin(),
+								linkPlugin(),
+								quotePlugin(),
+								thematicBreakPlugin(),
+								linkDialogPlugin(),
+								diffSourcePlugin(),
+								directivesPlugin({ directiveDescriptors: [AdmonitionDirectiveDescriptor] }),
+								imagePlugin(),
+								thematicBreakPlugin(),
+								tablePlugin(),
+								listsPlugin(),
+							]}
+						/>
+					</motion.div>
+				)}
+				<Quiz />
+				<Validation />
+			</motion.div>
+		</div>
 	);
 };
 
